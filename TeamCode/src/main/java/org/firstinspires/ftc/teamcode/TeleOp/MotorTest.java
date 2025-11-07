121e819625381c496e81f28bd47d3898ecb4a25b
Jacob Freeman <t00niyf5@duck.com>
2025-11-07 13:00:33 -0500

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MotorTest.java
@@ -29,6 +29,7 @@ public class MotorTest extends LinearOpMode {
         waitForStart();
         runtime.reset();
 
+        // Main branch test
         while (opModeIsActive()) {
             double power = gamepad1.left_stick_y;
             double power2 = gamepad1.right_stick_y;
