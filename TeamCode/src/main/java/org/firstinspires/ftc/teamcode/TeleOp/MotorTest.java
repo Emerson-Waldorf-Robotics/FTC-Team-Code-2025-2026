51ae136ac6bccd946a20a24a04f49c19a7359ba1
Jacob Freeman <t00niyf5@duck.com>
2025-12-12 18:15:33 -0500

    Make MotorTest support 4 motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MotorTest.java
@@ -15,12 +15,14 @@ public class MotorTest extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         final ElapsedTime runtime = new ElapsedTime();
 
-        DcMotor motorToRun = hardwareMap.get(DcMotor.class, "test_motor");
-        DcMotor motorToRun2 = hardwareMap.get(DcMotor.class, "test_motor2");
+        DcMotor motorToRun = hardwareMap.get(DcMotor.class, "left_front");
+        DcMotor motorToRun2 = hardwareMap.get(DcMotor.class, "left_back");
+        DcMotor motorToRun3 = hardwareMap.get(DcMotor.class, "right_front");
+        DcMotor motorToRun4 = hardwareMap.get(DcMotor.class, "right_back");
         
 
-        motorToRun.setDirection(DcMotorSimple.Direction.FORWARD);
-        motorToRun.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        // motorToRun.setDirection(DcMotorSimple.Direction.FORWARD);
+        // motorToRun.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         telemetry.addData("Status", "Initialized");
         telemetry.addLine("Remember to have the wheels form a cross across the body!!!");
@@ -32,12 +34,18 @@ public class MotorTest extends LinearOpMode {
         while (opModeIsActive()) {
             double power = gamepad1.left_stick_y;
             double power2 = gamepad1.right_stick_y;
+            double power3 = gamepad1.left_stick_x;
+            double power4 = gamepad1.right_stick_x;
 
             motorToRun.setPower(power);
             motorToRun2.setPower(power2);
+            motorToRun3.setPower(power3);
+            motorToRun4.setPower(power4);
 
             telemetry.addData("Motor 1", power * 100);
             telemetry.addData("Motor 2", power2 * 100);
+            telemetry.addData("Motor 3", power3 * 100);
+            telemetry.addData("Motor 4", power4 * 100);
 
             telemetry.update();
         }
