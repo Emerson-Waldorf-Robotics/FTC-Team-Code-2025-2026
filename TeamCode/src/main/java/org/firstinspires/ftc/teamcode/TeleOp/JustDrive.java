b104bcd155d63931c98f9f98d19d9c75108d649a
Jacob Freeman <t00niyf5@duck.com>
2025-12-12 18:14:33 -0500

    Make JustDrive use the shared code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/JustDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/JustDrive.java
@@ -29,11 +29,12 @@
 
 package org.firstinspires.ftc.teamcode.TeleOp;
 
-//import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import static org.firstinspires.ftc.teamcode.Shared.*;
 
 /*
  * This file contains an example of a Linear "OpMode".
@@ -68,30 +69,9 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class JustDrive extends LinearOpMode {
     @Override
     public void runOpMode() {
-        // Declare OpMode members for each of the 4 motors.
-        final ElapsedTime runtime = new ElapsedTime();
-
         // Initialize the hardware variables. Note that the strings used here must correspond
         // to the names assigned during the robot configuration step on the DS or RC devices.
-        DcMotor leftFrontDrive = hardwareMap.get(DcMotor.class, "left_front");
-        DcMotor leftBackDrive = hardwareMap.get(DcMotor.class, "left_back");
-        DcMotor rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front");
-        DcMotor rightBackDrive = hardwareMap.get(DcMotor.class, "right_back");
-
-        // ########################################################################################
-        // !!!            IMPORTANT Drive Information. Test your motor directions.            !!!!!
-        // ########################################################################################
-        // Most robots need the motors on one side to be reversed to drive forward.
-        // The motor reversals shown here are for a "direct drive" robot (the wheels turn the same direction as the motor shaft)
-        // If your robot has additional gear reductions or uses a right-angled drive, it's important to ensure
-        // that your motors are turning in the correct direction.  So, start out with the reversals here, BUT
-        // when you first test your robot, push the left joystick forward and observe the direction the wheels turn.
-        // Reverse the direction (flip FORWARD <-> REVERSE ) of any wheel that runs backward
-        // Keep testing until ALL the wheels move the robot forward when you push the left joystick forward.
-        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
-        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        hardwareInit(hardwareMap, telemetry, this::opModeIsActive);
 
         // Wait for the game to start (driver presses START)
         telemetry.addData("Status", "Initialized");
@@ -100,6 +80,8 @@ public class JustDrive extends LinearOpMode {
 
         waitForStart();
         runtime.reset();
+
+        //initMotors();
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
@@ -159,5 +141,7 @@ public class JustDrive extends LinearOpMode {
             telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
             telemetry.addLine("Remember to have the wheels form a cross across the body!!!");
             telemetry.update();
+
+            runCallbacks();
         }
     }}
